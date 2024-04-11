cead2d3ef07bd2791336fe7921c8984869e93a99
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-04-11 17:09:34 +0400

    Added -. Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>First Project Using Github Actions</h2>
+        <p>
+          Hey I am doing Deployment using GitHub Actions
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
